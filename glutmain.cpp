831c17688dda80b08d6814ea07312f4699abd6db
cxy166020 <cxy166020@gmail.com>
2011-08-09 14:06:45 -0700

    Fix viewing angle

--- glutmain.cpp
+++ glutmain.cpp
@@ -39,6 +39,7 @@ CMD2Model Ogro;
 CMD2Model Weapon;
 
 bool b_record = false;
+bool b_fbo    = false;
 
 bool  bAnimated	 = true;
 float angle      = 0.0;
@@ -60,6 +61,12 @@ float cx, cy, cz;
 
 // Camera principal axis (In world coordinate system)
 float px, py, pz;
+
+// Up vector of current view
+float ux, uy, uz;
+
+// Principal axis of first view
+float px_1, py_1, pz_1;
 
 // Focal length (In pixels)
 float cf;
@@ -104,11 +111,12 @@ void Model2World()
   
   yWorld[0] = tx; yWorld[1] = ty; yWorld[2] = tz;
   
-  // This is only temporary
+  // The program assumes that the first view is frontal parallel
+  // to the model
   // Note that the z axis of the model is sort of pointing back
   // to the camera, so the temporary vector is the negation of
   // principal axis
-  zWorld[0] = -px; zWorld[1] = -py; zWorld[2] = -pz;
+  zWorld[0] = -px_1; zWorld[1] = -py_1; zWorld[2] = -pz_1;
 
   cross(yWorld, zWorld, xWorld);
   cross(xWorld, yWorld, zWorld);
@@ -120,7 +128,7 @@ void Model2World()
   // Note that matrices in OpenGL are stored column-wise
   //
   // Use direction cosine to calculate the rotation matrix
-  //  - x2 -      -- cos(x2x1) cos(x2y1) cos(x2z1) --   - x1 -
+  //  - x2 -      |- cos(x2x1) cos(x2y1) cos(x2z1) -|   - x1 -
   //  | y2 |   =  |  cos(y2x1) cos(y2y1) cos(y2z1)  | * | y1 |
   //  - z2 -      |- cos(z2x1) cos(z2y1) cos(z2z1) -|   - z1 -
 
@@ -161,7 +169,7 @@ void record()
   if(!b_record)
     return;
 
-  unsigned char* depth = new unsigned char[window_width*window_height*4];
+  unsigned char* depth = new unsigned char[window_width*window_height];
 
   // glBindTexture(GL_TEXTURE_2D, depthTextureId);
   glReadPixels(0, 0, window_width, window_height, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, depth);
@@ -169,7 +177,7 @@ void record()
 
   ofstream ofm;
   ofm.open("test.dat", ios::binary | ios::trunc);
-  ofm.write((char*)depth, window_width*window_height*4);
+  ofm.write((char*)depth, window_width*window_height);
   ofm.close();
   
   delete[] depth;
@@ -184,7 +192,10 @@ void record()
 void Display( void )
 {
   // Rendre to FBO
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboId);	
+  bool temp_b_fbo = b_fbo;
+  
+  if (temp_b_fbo)
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboId);	
   
   // clear color and depth buffer
   glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
@@ -192,7 +203,7 @@ void Display( void )
 
 
   // gluLookAt(0.0, 0.0, 30.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-  gluLookAt(cx, cy, cz, ox, oy, oz, tx, ty, tz);
+  gluLookAt(cx, cy, cz, cx+px, cy+py, cz+pz, ux, uy, uz);
 
   CTimer::GetInstance()->Update();
   float timesec = CTimer::GetInstance()->GetTimeMSec() / 1000.0;
@@ -221,7 +232,8 @@ void Display( void )
   record();
 
   // back to normal window-system-provided framebuffer
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+  if (temp_b_fbo)
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
   
   // swap buffers and redisplay the scene
   glutSwapBuffers();
@@ -344,10 +356,10 @@ void Special( int key, int x, int y )
 
 void Keyboard(unsigned char key, int x, int y)
 {
-  if( key != 'd' )
-    return;
-
-  b_record = true;
+  if( key == ' ' )
+    b_fbo = !b_fbo;
+  else if( key != 'd' )
+    b_record = true;
 }
 
 void Idle()
@@ -379,7 +391,7 @@ int main( int argc, char *argv[] )
   ty = atof(argv[ArgCount++]);
   tz = atof(argv[ArgCount++]);
 
-  // 3D location of AR object origin
+  // 3D location of AR object origin (In world coordinate system)
   ox = atof(argv[ArgCount++]);
   oy = atof(argv[ArgCount++]);
   oz = atof(argv[ArgCount++]);
@@ -389,10 +401,20 @@ int main( int argc, char *argv[] )
   cy = atof(argv[ArgCount++]);
   cz = atof(argv[ArgCount++]);
 
-  // Camera principal axis (In world coordinate system)
+  // Principal axis of first view (In world coordinate system)
+  px_1 = atof(argv[ArgCount++]);
+  py_1 = atof(argv[ArgCount++]);
+  pz_1 = atof(argv[ArgCount++]);
+
+  // Principal axis of current view (In world coordinate system)
   px = atof(argv[ArgCount++]);
   py = atof(argv[ArgCount++]);
   pz = atof(argv[ArgCount++]);
+
+  // Up vector of current view (In world coordinate system)
+  ux = atof(argv[ArgCount++]);
+  uy = atof(argv[ArgCount++]);
+  uz = atof(argv[ArgCount++]);
 
   // Focal length (In pixels)
   cf = atof(argv[ArgCount++]);
